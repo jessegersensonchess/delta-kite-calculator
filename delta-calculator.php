5ea4274f197fdae715e7a897f58a5190d0286c98
jessegersensonchess <59052632+jessegersensonchess@users.noreply.github.com>
2025-10-20 08:49:26 +0200

    Refactor delta calculator with strict types and error handling
    Refactor delta calculator to improve error handling and calculations.

--- delta-calculator.php
+++ delta-calculator.php
@@ -1,50 +1,120 @@
 <?php
-error_reporting(0);
-if (isset($_POST['submit']))
-{
-    $noseangle = $_POST['noseangle'];
-    $lengthatcenterline = $_POST['lengthatcenterline'];
-    $units = $_POST['units'];
+declare(strict_types=1);
+
+// Show all errors during development
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+// Default values
+$noseangle = 96.0;
+$lengthatcenterline = 100.0;
+$units = 'cm';
+
+// Get input only if POST is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+    $input_noseangle = filter_input(INPUT_POST, 'noseangle', FILTER_VALIDATE_FLOAT);
+    $input_length = filter_input(INPUT_POST, 'lengthatcenterline', FILTER_VALIDATE_FLOAT);
+    $input_units = filter_input(INPUT_POST, 'units', FILTER_SANITIZE_STRING);
+
+    if ($input_noseangle !== false && $input_noseangle > 0 && $input_noseangle < 170) {
+        $noseangle = $input_noseangle;
+    }
+
+    if ($input_length !== false && $input_length > 0) {
+        $lengthatcenterline = $input_length;
+    }
+
+    if (!empty($input_units)) {
+        $units = $input_units;
+    }
 }
 
-$nominalspan = number_format((tan(deg2rad($noseangle / 2)) * ($lengthatcenterline * 2)) , 1, '.', '');
-$halfspan = number_format(($nominalspan / 2) , 1, '.', '');
-$lengthoffin = number_format((.75 * $lengthatcenterline) , 1, '.', '');
-$lengthofleadingedge = number_format(($lengthatcenterline / (cos(deg2rad($noseangle / 2)))) , 1, '.', '');
-$nominallengthofwingspars = number_format(($lengthofleadingedge * .75) , 1, '.', '');
+// --- CALCULATIONS ---
+$halfspan = calculateHalfSpan($noseangle, $lengthatcenterline);
+$nominalspan = $halfspan * 2;
+
+$lengthoffin = 0.75 * $lengthatcenterline;
+$cos_half_angle = cos(deg2rad($noseangle / 2));
+$lengthofleadingedge = ($cos_half_angle == 0) ? 0 : $lengthatcenterline / $cos_half_angle;
+
+$nominallengthofwingspars = $lengthofleadingedge * 0.75;
 $nosetoLEspar = $lengthofleadingedge - $nominallengthofwingspars;
-$depthoffin = number_format((.333 * $halfspan) , 1, '.', '');
-$spreaderstrutattachmentpoint = number_format(((7 / 9) * $nominallengthofwingspars) , 1, '.', '');
-$nosetospreader = number_format(cos(deg2rad($noseangle / 2)) * ($lengthofleadingedge - $spreaderstrutattachmentpoint) , 1, '.', '');
-$spreaderlength = number_format(2 / (cos(deg2rad($noseangle / 2)) / $nosetospreader) , 1, '.', '');
-$spreaderlength2 = number_format(($nosetospreader / $lengthatcenterline) * $nominalspan, 1, '.', '');
-if ($units == "in")
-{
-    $area = number_format((($halfspan / 12) * ($lengthatcenterline / 12)) , 1, '.', '');
-}
-else
-{
-    $area = number_format((($halfspan / 30.48) * ($lengthatcenterline / 30.48)) , 1, '.', '');
-}
-$line_strength_light_winds = number_format(($area * 1) , 0, '.', '');
-$line_strength_moderate_winds = number_format(($area * 2.2) , 0, '.', '');
-$line_strength_strong_winds = number_format(($area * 4) , 0, '.', '');
+$depthoffin = 0.333 * $halfspan;
+$spreaderstrutattachmentpoint = (7 / 9) * $nominallengthofwingspars;
 
-$keelfrontleg = sqrt(($depthoffin * $depthoffin) + (($lengthoffin - (0.5 * $lengthatcenterline)) * ($lengthoffin - (0.5 * $lengthatcenterline))));
-$keelrearleg = sqrt(($depthoffin * $depthoffin) + ((($lengthoffin - (0.5 * $lengthatcenterline)) - $lengthoffin) * (($lengthoffin - (0.5 * $lengthatcenterline)) - $lengthoffin)));
-$sewing = (2 * $lengthofleadingedge + (2 * $nosetoLEspar) + ($nominalspan) + (4.5 * $lengthatcenterline) + (2 * $keelrearleg) + (2 * $keelfrontleg));
+$nosetospreader = $cos_half_angle * ($lengthofleadingedge - $spreaderstrutattachmentpoint);
+
+// Protect division
+if ($cos_half_angle == 0 || $nosetospreader == 0) {
+    $spreaderlength = 'N/A';
+} else {
+    $spreaderlength = number_format(2 / ($cos_half_angle / $nosetospreader), 1, '.', '');
+}
+
+if ($lengthatcenterline == 0) {
+    $spreaderlength2 = 'N/A';
+} else {
+    $spreaderlength2 = number_format(($nosetospreader / $lengthatcenterline) * $nominalspan, 1, '.', '');
+}
+
+// Area (sq ft or m^2)
+$area = calculateArea($halfspan, $lengthatcenterline, $units);
+
+// Line strengths (lbs)
+$line_strength_light_winds = number_format($area * 1, 0, '.', '');
+$line_strength_moderate_winds = number_format($area * 2.2, 0, '.', '');
+$line_strength_strong_winds = number_format($area * 4, 0, '.', '');
+
+// Keel legs
+$keelfrontleg = calculateKeelLeg($depthoffin, $lengthoffin, $lengthatcenterline);
+$keelrearleg = calculateKeelLeg($depthoffin, $lengthoffin, $lengthatcenterline, true);
+
+// Total sewing length
+$sewing = calculateSewing($lengthatcenterline, $nominalspan, $lengthofleadingedge, $nosetoLEspar, $keelrearleg, $keelfrontleg);
+
+// Format keel lengths for display
 $keelfrontleg = number_format($keelfrontleg, 1, '.', '');
 $keelrearleg = number_format($keelrearleg, 1, '.', '');
-if ($noseangle)
-{
-    $noseangle = $noseangle;
-}
-else
-{
-    $noseangle = 96;
+
+// --- FUNCTIONS ---
+function calculateHalfSpan(float $noseangle, float $lengthatcenterline): float {
+    // half-span = tan(θ/2) × length at centerline
+    return tan(deg2rad($noseangle / 2)) * $lengthatcenterline;
 }
 
-?> 
+function calculateArea(float $halfspan, float $lengthatcenterline, string $units): float {
+    return match ($units) {
+        'in' => round(($halfspan / 12) * ($lengthatcenterline / 12), 1),
+        default => round(($halfspan / 30.48) * ($lengthatcenterline / 30.48), 1),
+    };
+}
+
+function calculateKeelLeg(float $depthoffin, float $lengthoffin, float $lengthatcenterline, bool $isRear = false): float {
+    $distance = $lengthoffin - (0.5 * $lengthatcenterline);
+    if ($isRear) {
+        $distance -= $lengthoffin;
+    }
+    return sqrt(($depthoffin ** 2) + ($distance ** 2));
+}
+
+function calculateSewing(float $lengthatcenterline, float $nominalspan, float $lengthofleadingedge, float $nosetoLEspar, float $keelrearleg, float $keelfrontleg): float {
+    return (
+        (2 * $lengthofleadingedge) +
+        (2 * $nosetoLEspar) +
+        $nominalspan +
+        (4.5 * $lengthatcenterline) +
+        (2 * $keelrearleg) +
+        (2 * $keelfrontleg)
+    );
+}
+?>
+
+
+
+
+
+
+
 
 <html><body><title>Delta kite calculator</title>
 <style>
@@ -72,73 +142,78 @@ else
 <h1>Delta kite calculator</h1>
 <p>Calculates a <a href="http://www.deltakites.com/plan.html">Dan Leigh standard delta</a>. Nose angles range from 86 to 120 degrees, typical is 96.</p>
 <p>How to use this form: Enter a nose angle and a center line length, then click 'calculate'.</p>
+
 <div id="form">
-	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-		<table align="center" >
-			<tr>
-				<td>nose angle</td>
-				<td><input id="noseangle" type="text" value="<? echo $noseangle; ?>" name="noseangle"></td>
-			</tr>
-			<tr>
-				<td nowrap>length at centerline (C)&nbsp;</td>
-				<td nowrap>
-					<input type="text" value="<? echo $lengthatcenterline; ?>" id="lengthatcenterline" name="lengthatcenterline"> 		
-					<select id="selectunits" name="units">
-						<option value="cm">cm</option>
-						<option <? 
-						if ($units == "in")
-						{
-						    echo "selected";
-						} 
-						?> value="in">in.
-						</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td align="center" colspan="2">
-					<input id="submitbutton" type="submit" name="submit" value="calculate">
-				</td>
-			</tr>
-		</table>
-	</form>
+    <form method="post" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+        <table align="center">
+            <tr>
+                <td>Nose angle</td>
+                <td>
+                    <input id="noseangle" type="text" name="noseangle" value="<?= htmlspecialchars((string)$noseangle) ?>">
+                </td>
+            </tr>
+            <tr>
+                <td nowrap>Length at centerline (C)</td>
+                <td nowrap>
+                    <input type="text" id="lengthatcenterline" name="lengthatcenterline" value="<?= htmlspecialchars((string)$lengthatcenterline) ?>">
+
+                    <select id="selectunits" name="units">
+                        <option value="cm" <?= $units === 'cm' ? 'selected' : '' ?>>cm</option>
+                        <option value="in" <?= $units === 'in' ? 'selected' : '' ?>>in.</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" colspan="2">
+                    <input id="submitbutton" type="submit" name="submit" value="Calculate">
+                </td>
+            </tr>
+        </table>
+    </form>
 </div>
+
 <div id="wrapper">
-	<div id="dimensions">
-		<h3>Dimensions</h3>
-		<table width="100%">
-			<?php
-			echo "<tr><td class='tdlabel'>C</td><td>length at centerline</td><td>" . $lengthatcenterline . "</td></tr>";
-			echo "<tr><td class='tdlabel'>S</td><td>nominal span</td><td>" . $nominalspan . "</td></tr>";
-			echo "<tr><td class='tdlabel'>S/2</td><td>nominal span (half)</td><td>" . $halfspan . "</td></tr>";
-			echo "<tr><td class='tdlabel'>F</td><td>length of fin</td><td>" . $lengthoffin . "</td></tr>";
-			echo "<tr><td class='tdlabel'>D</td><td>depth of fin</td><td>" . $depthoffin . "</td></tr>";
-			echo "<tr><td class='tdlabel'></td><td>length of fin side</td><td>" . $keelfrontleg . "</td></tr>";
-			echo "<tr><td class='tdlabel'></td><td>keel rear leg</td><td>" . $keelrearleg . "</td></tr>";
-			echo "<tr><td class='tdlabel'>B</td><td>towing point</td><td nowrap>" . (0.5 * $lengthatcenterline) . "</td></tr>";
-			echo "<tr><td class='tdlabel'>LE</td><td>length of Leading Edge</td><td>" . $lengthofleadingedge . "</td></tr>";
-			echo "<tr><td class='tdlabel'>L</td><td>length of wing spars</td><td>" . $nominallengthofwingspars . "</td></tr>";
-			echo "<tr><td class='tdlabel'></td><td>nose to LE spar</td><td>" . $nosetoLEspar . "</td></tr>";
-			echo "<tr><td nowrap class='tdlabel'>to SA</td><td>to spreader</td><td>" . $spreaderstrutattachmentpoint . "</td></tr>";
-			?>
-		</table>
-		<h3>Sail area</h3>
-		<?php echo $area . " sq ft" ?>
-		<h3>Recommended line</h3>
-		<?php
-			echo "light winds = " . $line_strength_light_winds . " lb. line<br/>";
-			echo "moderate winds = " . $line_strength_moderate_winds . " lb. line<br/>";
-			echo "strong winds = " . $line_strength_strong_winds . " lb. line<br/>";
-		?>
+    <div id="dimensions">
+        <h3>Dimensions</h3>
+        <table width="100%">
+            <tr><td class="tdlabel">C</td><td>Length at centerline</td><td><?= htmlspecialchars((string)$lengthatcenterline) ?></td></tr>
+            <tr><td class="tdlabel">S</td><td>Nominal span</td><td><?= number_format($nominalspan, 1) ?></td></tr>
+            <tr><td class="tdlabel">S/2</td><td>Half span</td><td><?= number_format($halfspan, 1) ?></td></tr>
+            <tr><td class="tdlabel">F</td><td>Length of fin</td><td><?= number_format($lengthoffin, 1) ?></td></tr>
+            <tr><td class="tdlabel">D</td><td>Depth of fin</td><td><?= number_format($depthoffin, 1) ?></td></tr>
+            <tr><td class="tdlabel"></td><td>Length of fin side (keel front leg)</td><td><?= htmlspecialchars($keelfrontleg) ?></td></tr>
+            <tr><td class="tdlabel"></td><td>Keel rear leg</td><td><?= htmlspecialchars($keelrearleg) ?></td></tr>
+            <tr><td class="tdlabel">B</td><td>Towing point</td><td><?= number_format(0.5 * $lengthatcenterline, 1) ?></td></tr>
+            <tr><td class="tdlabel">LE</td><td>Length of leading edge</td><td><?= number_format($lengthofleadingedge, 1) ?></td></tr>
+            <tr><td class="tdlabel">L</td><td>Length of wing spars</td><td><?= number_format($nominallengthofwingspars, 1) ?></td></tr>
+            <tr><td class="tdlabel"></td><td>Nose to LE spar</td><td><?= number_format($nosetoLEspar, 1) ?></td></tr>
+            <tr><td class="tdlabel" nowrap>to SA</td><td>Spreader attachment point</td><td><?= number_format($spreaderstrutattachmentpoint, 1) ?></td></tr>
+        </table>
+
+        <h3>Sail area</h3>
+        <p>
+            <?= htmlspecialchars((string)$area) ?> <?= $units === 'in' ? 'sq ft' : 'm²' ?>
+        </p>
+
+        <h3>Recommended line strength</h3>
+        <p>Light winds = <?= $line_strength_light_winds ?> lb. line</p>
+        <p>Moderate winds = <?= $line_strength_moderate_winds ?> lb. line</p>
+        <p>Strong winds = <?= $line_strength_strong_winds ?> lb. line</p>
+    </div>
+</div>
+
+
+
 		<h3>Resources</h3>
 		<a href="http://www.deltakites.com/plan.html">Plan and assembly instructions</a>
 		<br/>
 		<a href="http://www.jesseo.com/kites/">Other kites calculators</a>
 		<br/>
 		<a href="https://github.com/jessegersensonchess/delta-kite-calculator">Source code</a>
-		<br/><br/>
+		<br/>
+		<br/>
 	</div>
-	<div id="plan">	
+	<div id="plan">
 		<img src="dplan86.gif">
 	</div>
 </div>
